05af72377729abefea118a462afa059bd6101754
jricaur1 <julian.ric.ho@gmail.com>
2020-06-02 15:04:36 -0500

    commit inicial

---
+++ src/main.cc
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <cstdio>
+#include <math.h>
+#include <mpi.h>
+#include <omp.h>
+#include <assert.h>
+#include "predictions.h"
+
+int main(int argc, char** argv){
+    cout << prediction() << endl;
+}
---
+++ src/predictions.h
@@ -0,0 +1,6 @@
+#ifndef predictions_h
+#define predictions_h
+
+float[][] prediction();
+float[][] slidingWindow(float[][] matrix);
+#endif
