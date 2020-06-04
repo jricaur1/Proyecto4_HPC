160a0712ee89db87190f933457f0b39dcbfddd37
jricaur1 <julian.ric.ho@gmail.com>
2020-06-03 23:12:00 -0500

    pd&cd

--- src/main.cc
+++ src/main.cc
@@ -1,73 +1,132 @@
 #include <cstdlib>
 #include <cstdio>
-#include <omp.h>
-#include <mkl.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
-#include <algorithm>
+#include <sstream>
 
-void filter(const long n, const long m, float *data, const float threshold, std::vector<long> &result_row_ind);
+std::vector<std::string> readFile(std::string file_name, std::string actual_date);
+std::vector<std::string> split(std::string s, char delim);
+std::vector<std::string> previousDates7(std::vector<std::string> date);
+std::vector<std::string> previousDates14(std::vector<std::string> date);
 
-//reference function to verify data
-void filter_ref(const long n, const long m, float *data, const float threshold, std::vector<long> &result_row_ind) {
-  float sum;
-  for(long i = 0; i < n; i++){
-    sum = 0.0f;
-    for(long j = 0; j < m; j++) {
-      sum+=data[i*m+j];
-    } 
-    if(sum > threshold) 
-      result_row_ind.push_back(i);
+std::vector<std::string> previousDates7(std::vector<std::string> date){
+  std::vector<std::string> date7;
+  int day = std::stoi(date[1]);
+  int month = std::stoi(date[0]);
+  int year = std::stoi(date[2]);
+  for(int i = 0; i < 7; i++){
+    day--;
+    if(day == 0){
+      if(month == 1 || month == 5 || month == 7 || month == 10 || month == 12){
+        month--;
+        day = 30;
+      } else if(month == 2 || month == 4 || month == 6 || month == 8 || month == 9 || month == 11){
+        month--;
+        day = 31;
+      } else if (year % 4 == 0){
+        day = 29;
+      } else day = 28;
+      if(month == 0){
+        year--;
+        month = 12;
+        day = 31;
+      }
+    }
+    std::string s = std::to_string(month) + "/" + std::to_string(day) + "/" + std::to_string(year);
+    date7.push_back(s);
   }
-  std::sort(result_row_ind.begin(),result_row_ind.end());
+  return date7;
+}
+
+std::vector<std::string> previousDates14(std::vector<std::string> date){
+  std::vector<std::string> date14;
+  int day = std::stoi(date[1]);
+  int month = std::stoi(date[0]);
+  int year = std::stoi(date[2]) - 1;
+
+  for(int i = 0; i < 14; i++){
+    day--;
+    if(day == 0){
+      if(month == 1 || month == 5 || month == 7 || month == 10 || month == 12){
+        month--;
+        day = 30;
+      } else if(month == 2 || month == 4 || month == 6 || month == 8 || month == 9 || month == 11){
+        month--;
+        day = 31;
+      } else if (year % 4 == 0){
+        day = 29;
+      } else day = 28;
+      if(month == 0){
+        year--;
+        month = 12;
+        day = 31;
+      }
+    }
+    std::string s = std::to_string(month) + "/" + std::to_string(day) + "/" + std::to_string(year);
+    date14.push_back(s);
+  }
+  return date14;
+}
+
+std::vector<std::string> split(std::string s, char delim) {
+        std::stringstream ss(s);
+        std::string item;
+        std::vector<std::string> tokens;
+        while(getline(ss, item, delim)) {
+            tokens.push_back(item);
+        }
+        return tokens;
+    }
+ 
+
+std::vector<std::string> readFile(std::string file_name, std::string actual_date){
+  std::vector<std::string> record;
+  std::ifstream file;
+  file.open(file_name);
+  bool record_found = false;
+
+  std::string field_one;
+  std::string field_two;
+  std::string field_three;
+  std::string field_four;
+  std::string field_five;
+
+  while(getline(file, field_one, ',') && !record_found){
+    getline(file, field_two, ',');
+    getline(file, field_three, ',');
+    getline(file, field_four, ',');
+    getline(file, field_five, '\n');
+    if(field_one == actual_date){
+      record_found = true;
+      record.push_back(field_one);
+      record.push_back(field_two);
+      record.push_back(field_three);
+      record.push_back(field_four);
+      record.push_back(field_five);
+    }
+  }
+  return record;
 }
 
 int main(int argc, char** argv) {
-  float threshold = 0.5;
-  if(argc < 2) {
-    threshold = 0.5;
-  } else {
-    threshold = atof(argv[1]);
-  } 
-  
-  
-  const long n = 1<<15; //rows
-  const long m = 1<<18; //columns
-  
-  float *data = (float *) malloc((long)sizeof(float)*n*m);
-  long random_seed = (long)(omp_get_wtime()*1000.0) % 1000L;
-  VSLStreamStatePtr rnStream;
-  vslNewStream( &rnStream, VSL_BRNG_MT19937, random_seed);
-  
-  //initialize 2D data
-#pragma omp parallel for 
-  for(long i =0; i < n; i++)
-    vsRngUniform(VSL_RNG_METHOD_UNIFORM_STD, rnStream, m, &data[m*i], -1.0, 1.0);
+   std::vector<std::string> date = split("1/3/2019", '/');
+   std::vector<std::string> date7 = previousDates7(date);
+   std::vector<std::string> date14 = previousDates14(date);
+   std::vector<std::vector<std::string>> cd;
+   std::vector<std::vector<std::string>> pd;
 
-  std::vector<long> ref_result_row_ind; 
-  //compute the refernce data using unoptimized refernce function defined above
-  filter_ref(n, m, data, threshold, ref_result_row_ind);
-  
-  //compute actual data using the function defined in worker.cc and get the timing
-  std::vector<long> result_row_ind; 
-  const double t0 = omp_get_wtime();
-  filter(n, m, data, threshold, result_row_ind);
-  const double t1 = omp_get_wtime();
-  
-  //verify the actual data and the refernce data
-  if(ref_result_row_ind.size() != result_row_ind.size()) {
-    // Result sizes did not match
-    printf("Error: The reference and result vectors have different sizes: %ld %ld",ref_result_row_ind.size(), result_row_ind.size());
-  } else {
-    bool passed = true;
-    for(long i = 0; i < ref_result_row_ind.size(); i++) {
-      passed &= (ref_result_row_ind[i] == result_row_ind[i]);
-    } 
-    if(passed) {
-      // Printing perf
-      printf("Time: %f\n", t1-t0);
-    } else  {
-      // Results did not match
-      printf("Error: The reference and result vectors did not match");
-    }
-  }
+   for(int i = 0; i < 7; i++){
+    std::vector<std::string> s = readFile("../data/2169646.csv", date7[i]);
+      cd.push_back(s);
+      std::cout << cd[i][0];
+   }  
+   for(int i = 0; i < 14; i++){
+    std::vector<std::string> s = readFile("../data/2169646.csv", date14[i]);
+      cd.push_back(s);
+      std::cout << cd[i][0];
+   } 
+
+  return 0;
 }
