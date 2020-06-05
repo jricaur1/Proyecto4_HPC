6a0bb5b078af5988c57baf3936a1405dc0661c26
jricaur1 <julian.ric.ho@gmail.com>
2020-06-05 15:59:45 -0500

    Parallel V1

--- src/prediction.h
+++ src/prediction.h
@@ -4,13 +4,17 @@
 #include <cstdlib>
 #include <cstdio>
 #include <iostream>
+#include <omp.h>
 
 
-//omp directive to vectorize the function
+#pragma omp paralell
 std::vector<std::vector<float>> euclideanDistance(std::vector<std::vector<float>> &cd, std::vector<std::vector<float>> &pd);
 
+#pragma opm paralell
 std::vector<std::vector<float>> varianza(std::vector<std::vector<float>> diff);
 
+#pragma opm paralell
 std::vector<float> mediaVarianza(std::vector<std::vector<float>> cdv, std::vector<std::vector<float>> pdv);
 
+#pragma opm paralell
 std::vector<float> aproximacion(std::vector<float> diaA, std::vector<float> mediaV);
